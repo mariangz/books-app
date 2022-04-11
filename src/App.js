f7007b2778f3d8157988d7ec1508d2c2f05dfc3d
Mariano Giménez <marianosgimenez@gmail.com>
2022-04-11 15:09:23 +0200

    add footer closes #1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Container from './Container';
+import Footer from './Footer';
 
 function App() {
   return (
+    <Container>
+      <Footer />
+    </Container>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,86 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <h6>Column One</h6>
+        <ul>
+          <li>
+            <a>Consectetur</a>
+          </li>
+          <li>
+            <a>Adipiscing</a>
+          </li>
+          <li>
+            <a>Excepteur</a>
+          </li>
+          <li>
+            <a>lorem</a>
+          </li>
+          <li>
+            <a>Reprehenderit</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h6>Column Two</h6>
+        <ul>
+          <li>
+            <a>Pariatur</a>
+          </li>
+          <li>
+            <a>Minim</a>
+          </li>
+          <li>
+            <a>Enterprise</a>
+          </li>
+          <li>
+            <a>Commodo</a>
+          </li>
+          <li>
+            <a>Adipiscing</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h6>Column Three</h6>
+        <ul>
+          <li>
+            <a>Proident</a>
+          </li>
+          <li>
+            <a>Faucibus</a>
+          </li>
+          <li>
+            <a>Bibendum</a>
+          </li>
+          <li>
+            <a>Learn design</a>
+          </li>
+          <li>
+            <a>Adipiscing elit</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h6>Column Four</h6>
+        <ul>
+          <li>
+            <a>Lacus</a>
+          </li>
+          <li>
+            <a>Venenatis</a>
+          </li>
+          <li>
+            <a>Aliquet</a>
+          </li>
+          <li>
+            <a>Volutpat</a>
+          </li>
+          <li>
+            <a>Fames</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
