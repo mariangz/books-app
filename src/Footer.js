28db9776ace9a435d320798f3c8ab2a133cbd340
Mariano Giménez <marianosgimenez@gmail.com>
2022-04-13 12:59:12 +0200

    add new columns

--- src/Footer.js
+++ src/Footer.js
@@ -35,44 +35,150 @@ export default function Footer() {
         </ul>
       </div>
       <div>
-        <h6>About</h6>
+        <h6>Use Cases</h6>
         <ul>
           <li>
-            <a>Pariatur</a>
+            <a>UI design</a>
           </li>
           <li>
-            <a>Minim</a>
+            <a>UX design</a>
+          </li>
+          <li>
+            <a>Graphic design</a>
+          </li>
+          <li>
+            <a>Wireframing</a>
+          </li>
+          <li>
+            <a>Diagramming</a>
+          </li>
+          <li>
+            <a>Brainstorming</a>
+          </li>
+          <li>
+            <a>Templates</a>
+          </li>
+          <li>
+            <a>Remote design</a>
+          </li>
+          <li>
+            <a>Agencies</a>
+          </li>
+          <li>
+            <a>Figma for education</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h6>Explore</h6>
+        <ul>
+          <li>
+            <a>Design features</a>
+          </li>
+          <li>
+            <a>Prototyping features</a>
+          </li>
+          <li>
+            <a>Design systems features</a>
+          </li>
+          <li>
+            <a>Collaboration features</a>
+          </li>
+          <li>
+            <a>Design process</a>
+          </li>
+          <li>
+            <a>FigJam</a>
+          </li>
+          <li>
+            <a>Pricing</a>
           </li>
           <li>
             <a>Enterprise</a>
           </li>
           <li>
-            <a>Commodo</a>
+            <a>Customers</a>
           </li>
           <li>
-            <a>Adipiscing</a>
+            <a>Security</a>
+          </li>
+          <li>
+            <a>Integrations</a>
+          </li>
+          <li>
+            <a>Contact</a>
           </li>
         </ul>
       </div>
       <div>
-        <h6>Support</h6>
+        <h6>Resources</h6>
         <ul>
           <li>
-            <a>Proident</a>
+            <a>Blog</a>
           </li>
           <li>
-            <a>Faucibus</a>
+            <a>Best practices</a>
           </li>
           <li>
-            <a>Bibendum</a>
+            <a>Support</a>
+          </li>
+          <li>
+            <a>Developers</a>
           </li>
           <li>
             <a>Learn design</a>
           </li>
           <li>
-            <a>Adipiscing elit</a>
+            <a>Downloads</a>
+          </li>
+          <li>
+            <a>What's new</a>
+          </li>
+          <li>
+            <a>Releases</a>
+          </li>
+          <li>
+            <a>Careers</a>
+          </li>
+          <li>
+            <a>About us</a>
+          </li>
+          <li>
+            <a>Agency partners</a>
+          </li>
+          <li>
+            <a>Privacy</a>
+          </li>
+          <li>
+            <a>Status</a>
           </li>
         </ul>
+      </div>
+      <div>
+        <h6>Compare</h6>
+        <ul>
+          <li>
+            <a>Sketch</a>
+          </li>
+          <li>
+            <a>Adobe XD</a>
+          </li>
+          <li>
+            <a>Invision Studio</a>
+          </li>
+          <li>
+            <a>Framer</a>
+          </li>
+          <li>
+            <a>Design on Windows</a>
+          </li>
+          <li>
+            <a>Miro</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <a>Cookie settings</a>
       </div>
     </footer>
   );
