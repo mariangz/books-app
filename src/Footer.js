d2842f85ecce3059049ac77d819dfdc107d180e2
Mariano Giménez <marianosgimenez@gmail.com>
2022-04-20 15:55:23 +0200

    refactor: map links array

--- src/Footer.js
+++ src/Footer.js
@@ -7,174 +7,100 @@ export default function Footer() {
       <div className='footer__section'>
         <h6 className='footer__title'>Logo</h6>
         <ul>
-          <li>
-            <a className='footer__link'>
-              <FontAwesomeIcon icon={brands('facebook')} />
-              <span>Facebook</span>
-            </a>
-          </li>
-          <li>
-            <a className='footer__link'>
-              <FontAwesomeIcon icon={brands('twitter')} />
-              <span>Twitter</span>
-            </a>
-          </li>
-          <li>
-            <a className='footer__link'>
-              <FontAwesomeIcon icon={brands('youtube')} />
-              <span>Youtube</span>
-            </a>
-          </li>
-          <li>
-            <a className='footer__link'>
-              <FontAwesomeIcon icon={brands('instagram')} />
-              <span>Instagram</span>
-            </a>
-          </li>
-          <li></li>
+          {['Facebook', 'Twitter', 'Youtube', 'Instagram'].map((item) => (
+            <li key={item}>
+              <a className='footer__link'>
+                <FontAwesomeIcon icon={brands('facebook')} />
+                <span>{item}</span>
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
       <div className='footer__section'>
         <h6 className='footer__title'>Use Cases</h6>
         <ul>
-          <li>
-            <a className='footer__link'>UI design</a>
-          </li>
-          <li>
-            <a className='footer__link'>UX design</a>
-          </li>
-          <li>
-            <a className='footer__link'>Graphic design</a>
-          </li>
-          <li>
-            <a className='footer__link'>Wireframing</a>
-          </li>
-          <li>
-            <a className='footer__link'>Diagramming</a>
-          </li>
-          <li>
-            <a className='footer__link'>Brainstorming</a>
-          </li>
-          <li>
-            <a className='footer__link'>Templates</a>
-          </li>
-          <li>
-            <a className='footer__link'>Remote design</a>
-          </li>
-          <li>
-            <a className='footer__link'>Agencies</a>
-          </li>
-          <li>
-            <a className='footer__link'>Figma for education</a>
-          </li>
+          {[
+            'Use Cases',
+            'UI design',
+            'UX design',
+            'Graphic design',
+            'Wireframing',
+            'Diagramming',
+            'Brainstorming',
+            'Templates',
+            'Remote design',
+            'Agencies',
+            'Figma for education',
+          ].map((item) => (
+            <li key={item}>
+              <a className='footer__link'>{item}</a>
+            </li>
+          ))}
         </ul>
       </div>
       <div className='footer__section'>
         <h6 className='footer__title'>Explore</h6>
         <ul>
-          <li>
-            <a className='footer__link'>Design features</a>
-          </li>
-          <li>
-            <a className='footer__link'>Prototyping features</a>
-          </li>
-          <li>
-            <a className='footer__link'>Design systems</a>
-          </li>
-          <li>
-            <a className='footer__link'>Collaboration features</a>
-          </li>
-          <li>
-            <a className='footer__link'>Design process</a>
-          </li>
-          <li>
-            <a className='footer__link'>FigJam</a>
-          </li>
-          <li>
-            <a className='footer__link'>Pricing</a>
-          </li>
-          <li>
-            <a className='footer__link'>Enterprise</a>
-          </li>
-          <li>
-            <a className='footer__link'>Customers</a>
-          </li>
-          <li>
-            <a className='footer__link'>Security</a>
-          </li>
-          <li>
-            <a className='footer__link'>Integrations</a>
-          </li>
-          <li>
-            <a className='footer__link'>Contact</a>
-          </li>
+          {[
+            'Design features',
+            'Prototyping features',
+            'Design systems',
+            'Collaboration features',
+            'Design process',
+            'FigJam',
+            'Pricing',
+            'Enterprise',
+            'Customers',
+            'Security',
+            'Integrations',
+            'Contact',
+          ].map((item) => (
+            <li key={item}>
+              <a className='footer__link'>{item}</a>
+            </li>
+          ))}
         </ul>
       </div>
       <div className='footer__section'>
         <h6 className='footer__title'>Resources</h6>
         <ul>
-          <li>
-            <a className='footer__link'>Blog</a>
-          </li>
-          <li>
-            <a className='footer__link'>Best practices</a>
-          </li>
-          <li>
-            <a className='footer__link'>Support</a>
-          </li>
-          <li>
-            <a className='footer__link'>Developers</a>
-          </li>
-          <li>
-            <a className='footer__link'>Learn design</a>
-          </li>
-          <li>
-            <a className='footer__link'>Downloads</a>
-          </li>
-          <li>
-            <a className='footer__link'>What's new</a>
-          </li>
-          <li>
-            <a className='footer__link'>Releases</a>
-          </li>
-          <li>
-            <a className='footer__link'>Careers</a>
-          </li>
-          <li>
-            <a className='footer__link'>About us</a>
-          </li>
-          <li>
-            <a className='footer__link'>Agency partners</a>
-          </li>
-          <li>
-            <a className='footer__link'>Privacy</a>
-          </li>
-          <li>
-            <a className='footer__link'>Status</a>
-          </li>
+          {[
+            'Blog',
+            'Best practices',
+            'Support',
+            'Developers',
+            'Learn design',
+            'Downloads',
+            "What's new",
+            'Releases',
+            'Careers',
+            'About us',
+            'Agency partners',
+            'Privacy',
+            'Status',
+          ].map((item) => (
+            <li key={item}>
+              <a className='footer__link'>{item}</a>
+            </li>
+          ))}
         </ul>
       </div>
       <div className='footer__section'>
         <h6 className='footer__title'>Compare</h6>
         <ul>
-          <li>
-            <a className='footer__link'>Sketch</a>
-          </li>
-          <li>
-            <a className='footer__link'>Adobe XD</a>
-          </li>
-          <li>
-            <a className='footer__link'>Invision Studio</a>
-          </li>
-          <li>
-            <a className='footer__link'>Framer</a>
-          </li>
-          <li>
-            <a className='footer__link'>Design on Windows</a>
-          </li>
-          <li>
-            <a className='footer__link'>Miro</a>
-          </li>
+          {[
+            'Sketch',
+            'Adobe XD',
+            'Invision Studio',
+            'Framer',
+            'Design on Windows',
+            'Miro',
+          ].map((item) => (
+            <li key={item}>
+              <a className='footer__link'>{item}</a>
+            </li>
+          ))}
         </ul>
       </div>
       <div className='footer__section'>
