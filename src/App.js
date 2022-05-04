c4cea794cbb9d6323d7b196e2c418c23ec1fc96a
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-04 11:27:56 +0200

    feat: add login to add

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.scss';
 import './normalize.css';
 import Container from './Container';
 import SignupForm from './SignupForm/SignupForm';
+import Login from './Login/Login';
 import Footer from './Footer/Footer';
 
 function App() {
   return (
     <Container>
-      <SignupForm />
+      <Login />
       {/* <Footer /> */}
     </Container>
   );
