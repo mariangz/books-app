6cef3582ca9a846c33bb36f9ee0999451bd3f06a
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-04 10:06:48 +0200

    feat: fix eye-icon add info

--- src/FormComponents/Password/Password.js
+++ src/FormComponents/Password/Password.js
@@ -1,8 +1,7 @@
-// import React from 'react';
 import { useField } from 'formik';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro';
 import '../TextInput/TextInput.scss';
 import './Password.scss';
 import clsx from 'clsx';
@@ -18,28 +17,38 @@ export default function Password({ label, htmlFor, ...props }) {
   );
 
   return (
-    <div className='field password'>
+    <div className='field input-container'>
       <label htmlFor={htmlFor} className='label'>
         {label}
       </label>
-      <input
-        {...field}
-        {...props}
-        type={type}
-        className={clsx({
-          input: true,
-          'input-error': meta.error,
-          success: !meta.error,
-          touched: meta.touched,
-        })}
-      />
-      <span
-        className='eye-toggle'
-        onClick={() => setShowPassword((prevValue) => !prevValue)}
-      >
-        {icon}
-      </span>
-      {meta.touched && meta.error && <p className='error'>{meta.error}</p>}
+      <div className='info-icon__container'>
+        {meta.touched && meta.error && (
+          <FontAwesomeIcon icon={regular('circle-question')} className='icon' />
+        )}
+      </div>
+      <div className='password'>
+        <input
+          {...field}
+          {...props}
+          type={type}
+          className={clsx({
+            input: true,
+            'input-error': meta.error,
+            success: !meta.error,
+            touched: meta.touched,
+          })}
+        />
+        <span
+          className='eye-toggle'
+          onClick={() => setShowPassword((prevValue) => !prevValue)}
+        >
+          {icon}
+        </span>
+      </div>
+
+      {meta.touched && meta.error && (
+        <p className='error'>Password must include at least 8 characters.</p>
+      )}
     </div>
   );
 }
