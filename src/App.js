ff56e1919e99d4b578e53019b2cbe31f9679f469
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-05 09:43:44 +0200

    import BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.scss';
-import './normalize.css';
+import { BrowserRouter } from 'react-router-dom';
 import Container from './Container';
 import SignupForm from './SignupForm/SignupForm';
 import Login from './Login/Login';
 import Footer from './Footer/Footer';
 import Navbar from './Navbar/Navbar';
+import './App.scss';
+import './normalize.css';
 
 function App() {
   return (
