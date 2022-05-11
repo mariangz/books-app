ed7b7bf1b3dd84a65378ebef49ea67f54e058d15
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-11 16:12:38 +0200

    feat(App): add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './Components/Home';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import SignupForm from './Components/SignupForm/SignupForm';
 import Login from './Components/Login/Login';
+import Main from './Components/Main/Main';
 import Container from './Components/Container';
 import './App.scss';
 import './normalize.css';
@@ -13,7 +13,7 @@ function App() {
     <Container>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Main />} />
         <Route path='/signup' element={<SignupForm />} />
         <Route path='/login' element={<Login />} />
       </Routes>
