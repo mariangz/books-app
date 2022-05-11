122dc1a764e817605056a93cc50e7c6b539676ec
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-11 16:11:44 +0200

    add array data for main

---
+++ src/Components/Main/mainlist.js
@@ -0,0 +1,17 @@
+export const description = [
+  {
+    title: 'Read Free Books',
+    text: 'Millions of books available in our open library',
+    img: 'arrange',
+  },
+  {
+    title: 'Stay Organized',
+    text: 'Organize your books using our online shelves',
+    img: 'pileOfBook',
+  },
+  {
+    title: 'Share Your Thoughts',
+    text: 'Write a review and give them a rating',
+    img: 'reading',
+  },
+];
