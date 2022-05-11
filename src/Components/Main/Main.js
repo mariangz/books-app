828c44406d2fba7e717cc2ae2fdec1ebd91d3d2c
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-11 16:09:01 +0200

    feat(main): add images with description

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,34 @@
+import './Main.scss';
+import { description } from './mainlist';
+import arrange from '../../images/arrange.svg';
+import pileOfBook from '../../images/pileOfBook.svg';
+import reading from '../../images/reading.svg';
+
+export default function Main() {
+  return (
+    <main>
+      {/* {description.map((item) => (
+        <div>
+          <img src={'../../images/' + item.img + '.svg'} alt='' />
+          <h3>{item.title}</h3>
+          <p>{item.text}</p>
+        </div>
+      ))} */}
+      <div>
+        <img src={pileOfBook} alt='' />
+        <h3>Read Free Books</h3>
+        <p>Millions of books available in our open library</p>
+      </div>
+      <div>
+        <img src={arrange} alt='' />
+        <h3>Stay Organized</h3>
+        <p>Organize your books using our online shelves</p>
+      </div>
+      <div>
+        <img src={reading} alt='' />
+        <h3>Share Your Thoughts</h3>
+        <p>Write a review and give them a rating</p>
+      </div>
+    </main>
+  );
+}
