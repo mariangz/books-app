d09111c9907142c3f03da7dfbad9e1985f0bd628
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-11 16:10:58 +0200

    feat(navbar): add navlink routes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,8 @@
-import clsx from 'clsx';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import logo from '../../images/logo.svg';
+import search from '../../images/search.svg';
 import './Navbar.scss';
 
 export default function Navbar() {
@@ -9,22 +12,20 @@ export default function Navbar() {
         <nav className='navbar'>
           <ul className='header__links'>
             <li className='hero-logo push link'>
-              <NavLink
-                className={({ isActive }) => (isActive ? 'active' : '')}
-                to='/'
-              >
-                Home
+              <NavLink to='/'>
+                <img class='logo' src={logo} alt='' />
               </NavLink>
             </li>
             <li className='link'>
-              <NavLink to='signup'>Signup</NavLink>
+              <NavLink to='signup' className='button'>
+                Signup
+              </NavLink>
             </li>
             <li className='link'>
-              <NavLink to='login'>Login</NavLink>
+              <NavLink to='login' className='login'>
+                Log in
+              </NavLink>
             </li>
-            {/* <li className='link'>
-              <NavLink to={}> header link three</NavLink>
-            </li> */}
           </ul>
         </nav>
       </div>
