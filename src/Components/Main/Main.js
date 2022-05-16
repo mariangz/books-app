ed81229900ea00451c76e6f65570b0c14565f8b6
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-16 09:57:27 +0200

    refactor: add images from mainlist

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,34 +1,16 @@
 import './Main.scss';
 import { description } from './mainlist';
-import arrange from '../../images/arrange.svg';
-import pileOfBook from '../../images/pileOfBook.svg';
-import reading from '../../images/reading.svg';
 
 export default function Main() {
   return (
     <main className='main'>
-      {/* {description.map((item) => (
+      {description.map((item) => (
         <div>
           <img src={item.img} alt='' />
           <h3>{item.title}</h3>
           <p>{item.text}</p>
         </div>
-      ))} */}
-      <div>
-        <img src={pileOfBook} alt='' />
-        <h3>Read Free Books</h3>
-        <p>Millions of books available in our open library</p>
-      </div>
-      <div>
-        <img src={arrange} alt='' />
-        <h3>Stay Organized</h3>
-        <p>Organize your books using our online shelves</p>
-      </div>
-      <div>
-        <img src={reading} alt='' />
-        <h3>Share Your Thoughts</h3>
-        <p>Write a review and give them a rating</p>
-      </div>
+      ))}
     </main>
   );
 }
--- src/Components/Main/mainlist.js
+++ src/Components/Main/mainlist.js
@@ -1,17 +1,21 @@
+import arrange from '../../images/arrange.svg';
+import pileOfBook from '../../images/pileOfBook.svg';
+import reading from '../../images/reading.svg';
+
 export const description = [
   {
     title: 'Read Free Books',
     text: 'Millions of books available in our open library',
-    img: 'arrange',
+    img: arrange,
   },
   {
     title: 'Stay Organized',
     text: 'Organize your books using our online shelves',
-    img: 'pileOfBook',
+    img: pileOfBook,
   },
   {
     title: 'Share Your Thoughts',
     text: 'Write a review and give them a rating',
-    img: 'reading',
+    img: reading,
   },
 ];
