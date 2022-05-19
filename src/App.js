1b3d2f5c213290c44d4462749edc39c5bad348f2
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-19 14:56:02 +0200

    add new routes and components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { Route, Routes, useSearchParams } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import SignupForm from './Components/SignupForm/SignupForm';
@@ -6,18 +7,22 @@ import Login from './Components/Login/Login';
 import Main from './Components/Main/Main';
 import Container from './Components/Container';
 import BookCard from './Components/BookCard/BookCard';
+import GridBooks from './Components/GridBooks/GridBooks';
 import './App.scss';
 import './normalize.css';
 
 function App() {
+  const [user, setUser] = useState(false);
+
   return (
     <Container>
-      <Navbar />
+      <Navbar user={user} />
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/signup' element={<SignupForm />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login handleUser={setUser} />} />
         <Route path='/card' element={<BookCard />} />
+        <Route path='/books' element={<GridBooks user={user} />} />
       </Routes>
       <Footer />
     </Container>
