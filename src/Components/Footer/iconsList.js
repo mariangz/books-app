df91413f1d8f794e6939d27f410d6b97e8e5327f
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-19 14:55:05 +0200

    add icons for social media in footer

---
+++ src/Components/Footer/iconsList.js
@@ -0,0 +1,13 @@
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+
+export const icons = [
+  { name: 'Facebook', icon: faFacebook },
+  { name: 'Instagram', icon: faInstagram },
+  { name: 'Twitter', icon: faTwitter },
+  { name: 'Youtube', icon: faYoutube },
+];
