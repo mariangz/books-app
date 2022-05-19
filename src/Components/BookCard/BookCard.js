5837a9e0ac26c9f60037756d8834e6b037846597
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-19 14:56:20 +0200

    add bookCard component

--- src/Components/BookCard/BookCard.js
+++ src/Components/BookCard/BookCard.js
@@ -1,11 +1,19 @@
 import './BookCard.scss';
+import noBookCover from '../../images/noBookCover.png';
 
-export default function BookCard({ details }) {
+export default function BookCard(props) {
+  const info = props.details.volumeInfo;
   return (
-    <div>
-      <img alt='book' />
-      <h3>Title</h3>
-      <p>Autor</p>
+    <div className='bookCard'>
+      {info.imageLinks?.thumbnail ? (
+        <img className='coverImg' src={info.imageLinks?.thumbnail} alt='book' />
+      ) : (
+        <img className='coverImg' src={noBookCover} alt='book' />
+      )}
+      <div className='text'>
+        <h3 className='author'>{info.title}</h3>
+        <p>{info.authors}</p>
+      </div>
     </div>
   );
 }
