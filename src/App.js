bf661e0a580dde220746335baed15086b5300fe7
Mariano Giménez <marianosgimenez@gmail.com>
2022-05-27 12:03:33 +0200

    feat: add protected route

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import SignupForm from './Components/SignupForm/SignupForm';
 import Login from './Components/Login/Login';
 import Main from './Components/Main/Main';
 import Container from './Components/Container';
+import ProtectedRoute from './Components/ProtectedRoute';
 import BookCard from './Components/BookCard/BookCard';
 import GridBooks from './Components/GridBooks/GridBooks';
-import { UserContext } from './UserContext';
 import { UserProvider } from './UserContext';
+import { UserContext } from './UserContext';
 import { useContext } from 'react';
+
 import './App.scss';
 import './normalize.css';
 function App() {
-  // const [user, setUser] = useContext(UserContext);
-
   return (
     <UserProvider>
       <Container>
@@ -24,7 +24,9 @@ function App() {
           <Route path='/signup' element={<SignupForm />} />
           <Route path='/login' element={<Login />} />
           <Route path='/card' element={<BookCard />} />
-          <Route path='/books' element={<GridBooks />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path='/books' element={<GridBooks />} />
+          </Route>
         </Routes>
         <Footer />
       </Container>
